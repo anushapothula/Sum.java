b5690d4ba38be1e157fcd7de969e7882d8c31f5a
anushapothula <83360113+anushapothula@users.noreply.github.com>
2021-04-30 14:35:03 +0530

    Update and rename README.md to Sum.java

---
+++ Sum.java
@@ -0,0 +1,12 @@
+ class Sumnum{
+ public statuc void main(String[] args){
+ Scanner sc=new Scanner(System.in); 
+Syste.out.println("enter the value of n"); 
+int n=sc.nextInt();
+ int sum=0; 
+for(int i=;i&lt;=n;i++)
+ sum=i++; 
+if(c&lt;=range){ 
+System.out.println(sum); 
+ }
+ }
